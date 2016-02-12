c8c7b7e61105e9c1d791547ab84a7ff0a6783b62
Yuriy Denyshchenko <ydenyshchenk@ebay.com>
2016-02-12 14:11:40 +0200

    Added store attachment

--- app/code/local/Devils/HomeWidget/Block/Widget/Home.php
+++ app/code/local/Devils/HomeWidget/Block/Widget/Home.php
@@ -6,7 +6,12 @@ class Devils_HomeWidget_Block_Widget_Home extends Mage_Core_Block_Template imple
     {
         /** @var Devils_HomeWidget_Model_Resource_Image_Collection $collection */
         $collection = Mage::getModel('devils_homewidget/image')->getCollection();
-        $collection->addFieldToFilter('is_active', 1)->setOrder('position', 'ASC')->setOrder('entity_id', 'ASC');
+        $store = Mage::app()->getStore();
+        $collection
+            ->addStoreFilter($store)
+            ->addFieldToFilter('is_active', 1)
+            ->setOrder('position', 'ASC')
+            ->setOrder('entity_id', 'ASC');
 
         return $collection;
     }
